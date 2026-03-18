29767a46230cae509b614e26e75386dc2fe9b9a1
aczee <aczee@ADAM>
2026-03-18 08:46:21 +0100

    elozohoz elfelejtettem odairni a keresztrejtven racs letrehozasat de minden feladathoz megcsinaltam a konzolos kiirast

--- Keresztrejtveny/Program.cs
+++ Keresztrejtveny/Program.cs
@@ -7,6 +7,13 @@
         {
             KeresztrejtenyRacs racs = new KeresztrejtenyRacs("kr1.txt");
             Console.WriteLine("5. feladat");
+            Console.WriteLine($"a keresztrejtveny merete: {racs.MeretSzoveg()}");
+
+            Console.WriteLine("6. feladat");
+            racs.SzavakStatisztika(out int szavakSzama, out int maxHossz);
+            Console.WriteLine($"a keresztrejtvenyben {szavakSzama} darab szo van.");
+            Console.WriteLine($"a leghosszabb szo hossza: {maxHossz}");
+
 
             Console.ReadKey();
         }
