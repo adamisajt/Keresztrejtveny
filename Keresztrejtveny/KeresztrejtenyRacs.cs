6fe1a689d1aa4c6d72beed7ffc942a39f0b84459
aczee <aczee@ADAM>
2026-03-18 08:23:40 +0100

    Add project files.

---
+++ Keresztrejtveny/KeresztrejtenyRacs.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Keresztrejtveny
+{
+    internal class KeresztrejtenyRacs
+    {
+        public List<string> Adatsorok { get; private set; } = new List<string>();
+        public char[,] Racs { get; private set; }
+        public int SorokDb { get; private set; }
+        public int OszlopokDb { get; private set; }
+
+    }
+}
